9a062edb1daaa5599eef73fd978e1ba57d30e538
Aaron Myrie <aaron477myrie@gmail.com>
2019-01-30 22:13:04 +0000

    made adjustment

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ app = Flask(__name__)
 # Put your routes below this comment
 '''
 @app.route('/')
+#def home():
+    #return 'My home page'
+
 @app.route('/about')
 def about():
     return render_template('about.html') 
